4cf9d5e5dda4b31347eabd879eb7494271631f91
talahanakrajin <athallahraja8@gmail.com>
2026-02-28 13:48:13 +0700

    some thing happened

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDJ36ADMSgVtgkVeG_11Nmm2NmwMEnh33g",
-  authDomain: "fir-auth-athallah.firebaseapp.com",
-  projectId: "fir-auth-athallah",
-  storageBucket: "fir-auth-athallah.firebasestorage.app",
-  messagingSenderId: "34088031187",
-  appId: "1:34088031187:web:c292e4a8898e289ba21526"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
